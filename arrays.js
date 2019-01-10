ac4be17f43a984f772a8c8475d41da65766b016c
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-10 08:52:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,10 +29,7 @@ function removeElementFromBeginningOfArray(array){
   
   return array.slice(1)
 }
-function destructivelyRemoveElementFromEndOfArray(array){
-  
-  return array.pop()
-}
+
 
 function removeElementFromEndOfArray(array){
   
