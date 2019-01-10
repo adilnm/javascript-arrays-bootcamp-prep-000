77dcd73574c2706595b0d1b1988aad3375c0effd
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-10 08:09:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -44,3 +44,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift()
 }
 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+  return array.shift()
